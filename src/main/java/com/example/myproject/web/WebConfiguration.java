041475a0321e77fad0eb1b11faa257793620e66b
dingdongsheng <dingdongsheng@qima-inc.com>
2016-04-07 09:53:16 +0800

    add h2 and console

---
+++ src/main/java/com/example/myproject/web/WebConfiguration.java
@@ -0,0 +1,20 @@
+package com.example.myproject.web;
+
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+/**
+ * Created by dingdongsheng on 16/4/7.
+ */
+@Configuration
+public class WebConfiguration {
+    @Bean
+    ServletRegistrationBean h2servletRegistration() {
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(new WebServlet());
+        registrationBean.addUrlMappings("/console/*");
+        return registrationBean;
+    }
+}
